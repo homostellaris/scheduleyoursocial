f61f8182b1ac8a4f2ff1774c1b73d83ad34ab975
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-01-09 21:40:09 +0000

    Add comparison to howbout
    Hoping that this will attract more organic traffic.

---
+++ src/routes/alternative-to/howbout.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export const prerender = true
+</script>
+
+<svelte:head>
+	<title>Alternative to Howbout</title>
+</svelte:head>
+
+<h2>Schedule Your Social is similar to other social planning apps like Howbout</h2>
+<p>However we solve a slightly different use case to Howbout and differ in a few key ways.</p>
+<ul>
+	<li>Schedule Your Social aims to be as simple and quick to use as possible in contrast to Howbout which was more features but might take longer to arrive at a time for your social.</li>
+	<li>Schedule Your Social only allows you to agree a date to meet, not a time.</li>
+	<li>Schedule Your Social provides real-time updates so you don't need to refresh the page!</li>
+	<li>Schedule Your Social is not a mobile app, its a web app, but it still works great on mobile.</li>
+</ul>
+<a href="/">Get started!</a>
+<!-- TODO: Can I do a sweet transition from this page to the next? -->
