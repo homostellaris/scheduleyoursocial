c8e1a0c710c905c7a49d053436c0fdab8278f783
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-04-30 22:07:52 +0100

    Add 'free' landing page

---
+++ src/routes/free.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export const prerender = true
+</script>
+
+<svelte:head>
+	<title>No signup. No ads. The free, fast, and fun way to agree a day for your social.</title>
+	<meta name="description" content="Schedule Your Social is the social planning app that requires no signup, is ad-free, and fast! We help you find a day for your social in minutes.">
+</svelte:head>
+
+<h1>Schedule Your Social</h1>
+<h2>The fastest way to find out when your friends are free</h2>
+<p>Schedule Your Social is the social planning app that requires no signup, is ad-free, and fast! We help you find a day when everyone is free for your social event in minutes.</p>
+<a href="/">Get started!</a>
+<!-- TODO: Can I do a sweet transition from this page to the next? -->
