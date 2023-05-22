f05910f8c984bf91e6b8fbf666b9c01a5e0a06dc
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-05-22 22:02:51 +0100

    Add organize dates with friends landing page
    This is the first blog post style landing page

--- src/lib/LandingPage.svelte
+++ src/lib/LandingPage.svelte
@@ -1,25 +1,47 @@
 <script context="module">
-  import LandingPage from '$lib/LandingPage.svelte'
-
-	export const prerender = true
+  export const prerender = true
 </script>
 
 <script>
-	export let title
-	export let description
+  export let title
+  export let description
 </script>
 
 <svelte:head>
-	<title>{title}</title>
-	<meta name="description" content="{description}">
+  <title>{title}</title>
+  <meta name="description" content={description} />
 </svelte:head>
 
-<h1>The fastest way to find out when your friends are free</h1>
-<p>Schedule Your Social is the social planning app that has no signup, no ads, and is completely free. We help you find a date that works for everyone in three easy steps.</p>
+<div class="content">
+  <slot />
+</div>
+
+<h2>The fastest way to find out when your friends are free</h2>
+<p>
+  Schedule Your Social is the social planning app that has no signup, no ads, and is completely
+  free. We help you find a date that works for everyone in three easy steps.
+</p>
 <ol>
-	<li>Enter your name and availability 📅</li>
-	<li>Share the link with your friends 🔗</li>
-	<li>Choose the best date ✅</li>
+  <li>Enter your name and availability 📅</li>
+  <li>Share the link with your friends 🔗</li>
+  <li>Choose the best date ✅</li>
 </ol>
 
 <a href="/">Get started!</a>
+
+<style>
+  .content :global(p),
+  .content :global(h2) {
+    text-align: left;
+    max-width: unset;
+  }
+
+  .content :global(h1) {
+    text-align: center;
+  }
+
+  .content :global(img) {
+    margin: 1rem auto;
+    display: block;
+  }
+</style>
