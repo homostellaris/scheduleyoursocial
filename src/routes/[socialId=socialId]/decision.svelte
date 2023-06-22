2be070f77c3fff076370c423d03ccbb338f9a8b6
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-06-22 23:37:21 +0100

    Add review CTA on decision page

--- src/routes/[socialId=socialId]/decision.svelte
+++ src/routes/[socialId=socialId]/decision.svelte
@@ -1,18 +1,23 @@
 <script context="module">
-	import Retreat from "$lib/Back.svelte"
+  import Retreat from "$lib/Back.svelte"
 </script>
 
 <script>
-	export let social
-	let decision
-	$: decision = new Date(social.decision).toLocaleDateString(undefined, {
-		weekday: 'long',
-		month: 'long',
-		day: 'numeric',
-	})
+  export let social
+  let decision
+  $: decision = new Date(social.decision).toLocaleDateString(undefined, {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  })
 </script>
 
 <h1>You're social is on {decision}</h1>
-<p>Have fun 🎉 and if you found this app helpful then please share it with others who might find it helpful too 🙏</p>
+<p>
+  Have fun 🎉 and if you found this app helpful then please share it with others or <a
+    href="https://www.producthunt.com/products/schedule-your-social/reviews/new"
+    >leave us a review</a
+  > 🙏
+</p>
 
-<Retreat back="everyone"/>
+<Retreat back="everyone" />
