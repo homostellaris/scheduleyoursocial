ea1d510a032a7484157135b3ce9c470022603c72
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-07-07 22:50:11 +0100

    Fix ES module import error in deployed function

--- src/routes/with/[slug]/[socialId=socialId]/invite.json.js
+++ src/routes/with/[slug]/[socialId=socialId]/invite.json.js
@@ -1,5 +1,5 @@
 import faunadb from 'faunadb'
-import {toDatabaseId} from '$lib/id'
+import { toDatabaseId } from '$lib/id'
 
 const q = faunadb.query
 
@@ -10,12 +10,11 @@ const client = new faunadb.Client({
 	secret: process.env.FAUNADB_SERVER_SECRET,
 })
 
-export async function post ({locals, params, request}) {
+export async function post ({ locals, params, request }) {
 	const socialId = params.socialId
 	const reference = toDatabaseId(socialId)
 
 	const data = await request.formData()
-	console.log(data)
 	const name = data.get('name')
 
 
