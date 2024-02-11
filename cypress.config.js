4d03a81825d550a117b6cafb4436dd2cc36973a6
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-02-11 13:38:36 +0000

    ⬆️ Upgrade to Node 20
    Upgrading Node to support Sentry 7 which allows
    the user feedback dialog.

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress'
+import plugins from './cypress/plugins/index.cjs'
+
+export default defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return plugins(on, config)
+    },
+    baseUrl: 'http://localhost:6602',
+  },
+})
