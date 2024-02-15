1f5e79297e774c5c7c213179793455df4f368da3
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-02-15 21:36:17 +0000

    🔥 Remove help and about page
    These page aren't doing much so would prefer to de-clutter the footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -8,8 +8,6 @@
   </div>
   <!-- TODO: Use something like `class:active={$page.path === '/about'}` to indicate which page we're currently on. -->
   <span><a sveltekit:prefetch href="/">Schedule another social</a></span>
-  <span><a sveltekit:prefetch href="/help">Help</a></span>
-  <span><a sveltekit:prefetch href="/about">About</a></span>
   <span><a sveltekit:prefetch href="/privacy">Privacy</a></span>
 </footer>
 
