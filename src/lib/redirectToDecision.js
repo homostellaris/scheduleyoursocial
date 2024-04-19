3dd0fa73cd5bcd54ea0debd95d411cbe972a8baf
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-04-19 22:43:54 +0100

    🐛 Redirecting to decision when no decision

--- src/lib/redirectToDecision.js
+++ src/lib/redirectToDecision.js
@@ -4,7 +4,7 @@ import {redirect} from '@sveltejs/kit'
 export function hasDecisionBeenSeen(request, social) {
   const cookieHeader = request.headers.get('cookie')
   const cookies = cookie.parse(cookieHeader || '')
-  return social.decision !== cookies.decision
+  return social.decision && social.decision !== cookies.decision
 }
 
 export function decisionRedirect(socialId) {
