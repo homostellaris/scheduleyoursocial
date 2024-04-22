c4b8eff4ee8b8764c2849c55b6fa296adc7c9211
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-04-22 13:28:26 +0100

    📈 Add ref to host link

--- src/routes/with/[host]/+page.svelte
+++ src/routes/with/[host]/+page.svelte
@@ -6,12 +6,18 @@
 
   /** @type {import('./$types').PageData} */
   export let data
-  console.log(data)
+
   let loading
 
   onMount(() => {
     document.getElementById('name').focus()
   })
+
+  function prepareLink(link) {
+    const url = new URL(link)
+    url.searchParams.set('ref', 'schedule-your-social')
+    return url.href
+  }
 </script>
 
 <svelte:head>
@@ -22,8 +28,10 @@
 <h1>Schedule your social</h1>
 <h2 class="font-secondary">
   <!-- TODO: Find out how to make target blank work -->
-  with {#if data.link}<a href={data.link} target="_blank" class="font-secondary"
-      >{data.host}</a
+  with {#if data.link}<a
+      href={prepareLink(data.link)}
+      target="_blank"
+      class="font-secondary">{data.host}</a
     >{:else}{data.host}{/if}
 </h2>
 <p>Let's start with your name</p>
