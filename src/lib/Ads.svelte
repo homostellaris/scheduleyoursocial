d5de18fea2139a7a1406fbec3d207a020675678a
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-08-02 22:26:35 +0100

    🐛 Amazon associates link not redirecting properly
    Link generated from US store seems to redirect better

--- src/lib/Ads.svelte
+++ src/lib/Ads.svelte
@@ -1,5 +1,5 @@
 <script>
-  import logo from '$lib/liquid death.jpg'
+  import liquidDeath from '$lib/liquid death.jpg'
   import {getContext} from 'svelte'
 
   const {getAnalytics} = getContext('analytics')
@@ -15,7 +15,7 @@
   support development.
 </p>
 <a
-  href="https://amzn.to/4dpshsU"
+  href="https://www.amazon.com/Liquid-Death-Squeezed-Sparkling-Sweetened/dp/B0CV4MCM91?crid=22U074H84ZC36&dib=eyJ2IjoiMSJ9.pT7IMTvtK8Opux2I83EFFj5eG6XaUv8Unt-qB2VpoikB85kTSjJ26A00VmKb0DCghWD-Txi9hc8ziGpGEOeR_veM1PEOrkuVWbYRcPNyOlfGT7001KpDKfHBNY-_inMEbpzExnzN6UENQxLqSQsW1YidM7D_L-pVM7bzCLj27uuBcHDRcZTv5B06BxP6KrkH3O1HeCGtbRFyLrrULtgsagw4pDDCNDcmbjSWHZUL7zDUtuI0t7nAM3blY_KuwygnHa1AWAf8QIQub-hijptbBjBlooK3H9fckwAGn4vBO7k._Z43AazpptNX9xPxKKmv9KGepQvyQ1eIXJqtQx_oaLg&dib_tag=se&keywords=liquid+death&qid=1722633813&sprefix=liquid+dea%2Caps%2C279&sr=8-1&linkCode=ll1&tag=scheduleyours-20&linkId=8676ff9e6324d307e0c9190024e709b5&language=en_US&ref_=as_li_ss_tl"
   on:click={() => {
     getAnalytics().trackEvent('Liquid Death link clicked')
   }}
@@ -23,7 +23,7 @@
 >
   <figure>
     <img
-      src="https://m.media-amazon.com/images/I/71pCzc0NH4L.jpg"
+      src={liquidDeath}
       alt="Liquid Death Still Mountain Water, 12 x 500 ml"
       style:max-height="20vh"
       style:margin="auto"
